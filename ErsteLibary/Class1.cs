a7f3100099192f9a9ee289798ea40a52a1705d02
Helge_ <helgehecht92@gmail.com>
2026-03-31 13:14:45 +0200

    Test

---
+++ ErsteLibary/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace ErsteLibary;
+
+public class Rechner
+{
+    public int Addiere(int a,int b) => a+b;
+    public int Addieren2(int a, int b)
+    {
+        Console.WriteLine("Addieren2 wird aufgerufen");
+        return a + b;
+    }
+
+}
+public static class RechnerStat
+{
+    public static int Addiere(int a, int b) => a + b;
+
+    public static int Addiere2(int a, int b)
+    {
+        Console.WriteLine("_____    Addiere statisch    ______");
+        return a + b;
+    }
+}
+
+
---
+++ TestLib/Libtest/Program.cs
@@ -0,0 +1,28 @@
+﻿using ErsteLibary;
+
+namespace TesteLib
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            // Instanz der Klasse Rechner erstellen
+           var r = new Rechner();
+           int ergebnis =r.Addiere2(5,7);
+           Console.WriteLine("Ergebnis Addition: " + ergebnis);
+
+
+           //Statischer Aufruf
+           Console.WriteLine("Addition mit Aufruf " + RechnerStat.Addiere(3, 4));
+              Console.WriteLine("Addition mit Aufruf " + RechnerStat.Addiere2(3, 4));
+              
+           
+        }  
+
+        
+    }
+    
+}
