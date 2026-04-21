2bc8772e66647264ed72ef00d53ff856283037a9
Helge_ <helgehecht92@gmail.com>
2026-04-21 14:55:34 +0200

    EventÜbung

---
+++ Übungen/events/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace events
+{
+    public class Zähler
+    {
+        private int _total;
+        private int _threshold;
+
+        public Zähler(int threshold) => _threshold = threshold;
+
+        public event EventHandler ThresholdReached;
+        public void Add(int x)
+        {
+            _total += x => _total >= _threshold? ThresholdReached?.Invoke(this,EventArgs.Empty):null;
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            var zähler = new Zähler(5);
+            zähler.ThresholdReached += (sender,e) => Console.WriteLine("Threshold reached!");
+            zähler.Add(3);
+            zähler.Add(2); // Abhier wäre die Ausgabe "Threshold reached!" zu sehen, da der Schwellenwert erreicht wurde.
+        }
+    }
+}
---
+++ Übungen/events/Userspecific Data/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Userspecific_Data
+{
+    public class ThresholdReachedEventArgs : EventArgs
+    {
+        public int Threshold {get;set;}
+        public int Value {get;set;}
+        public DateTime TimeReached{get;set;}
+
+    }
+
+    public class Zähler
+    {
+        private int _total;
+        private int _thershold;
+
+        public Zähler(int threshold) => _thershold = threshold; 
+
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+
+        public void Add(int x)
+        {
+            _total += x;
+            if(_total >= _thershold)
+            {
+                OnThresholdReached(new ThresholdReachedEventArgs{Threshold = _thershold, Value = _total, TimeReached = DateTime.Now});
+            } 
+
+        }
+        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+        {
+            ThresholdReached?.Invoke(this,e);
+        }
+
+        class Programm
+        {
+            public static void Main()
+            {
+                var zähler = new Zähler(5);
+                zähler.ThresholdReached += (sender,e) =>
+                Console.WriteLine($"Ziel Erreicht! Schwellenwert: {e.Threshold} erreicht! Wert: {e.Value} Zeit: {e.TimeReached}");
+                zähler.Add(6);
+            }
+        }
+    }
+}
