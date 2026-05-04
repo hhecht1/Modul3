ba97f6f09341c2caf8f4ccea49ecf0e1a74c2e9d
Helge_ <helgehecht92@gmail.com>
2026-05-04 09:09:33 +0200

    lib

--- ErsteLibary/obj/Debug/net10.0/ErsteLibary.AssemblyInfo.cs
+++ ErsteLibary/obj/Debug/net10.0/ErsteLibary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ErsteLibary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc8772e66647264ed72ef00d53ff856283037a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6cf4cc0aa30fcca8f0bba1dd79e7661703dd81")]
 [assembly: System.Reflection.AssemblyProductAttribute("ErsteLibary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ErsteLibary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
